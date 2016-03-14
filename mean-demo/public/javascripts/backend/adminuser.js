a7044382b668220d88cb91a27443beedb1dbd1ba
Jacky Wu <wujiajun@lyancoffee.com>
2016-03-14 11:27:00 +0800

    增加后台管理账户模板

---
+++ mean-demo/public/javascripts/backend/adminuser.js
@@ -0,0 +1,32 @@
+var app = angular.module('backend', ['ngResource', 'ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/template/backend/adminuser/list.html',
+      controller: 'HomeCtrl'
+    })
+	.when('/add', {
+      templateUrl: '/template/backend/adminuser/create.html',
+      controller: 'AddCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+}]);
+
+app.controller('HomeCtrl', ['$scope', '$resource',
+  function($scope, $resource) {
+    // var Videos = $resource('/api/videos');
+    // Videos.query(function(videos) {
+    //   $scope.videos = videos;
+    // });
+  }
+]);
+
+app.controller('AddCtrl', ['$scope', '$resource', '$location',
+  function($scope, $resource, $location) {
+    $scope.save = function() {
+
+    };
+  }
+]);
